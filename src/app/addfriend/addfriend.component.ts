0e48d79ff8533f54b8978832f17f988ab426afb2
Nivedhya41 <nivedhya61@gmail.com>
2022-07-22 19:25:42 +0530

    friendapp-frontend

---
+++ src/app/addfriend/addfriend.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FriendviewService } from '../friendview.service';
+
+@Component({
+  selector: 'app-addfriend',
+  templateUrl: './addfriend.component.html',
+  styleUrls: ['./addfriend.component.css']
+})
+export class AddfriendComponent implements OnInit {
+  friend={
+    name:"",
+    friendname:"",
+    friendNickname:"",
+    DescribeYourFriend:""
+
+
+
+}
+
+
+  constructor(private friendservice:FriendviewService) { }
+
+  ngOnInit(): void {
+  }
+  Addfriend(){
+    this.friendservice.Addfriend(this.friend).subscribe((data)=>{
+      alert("Success")
+    })
+    alert("Success");
+
+  }
+
+}
